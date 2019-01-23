1c000550c9dbe34977fd1270c7f497e1c5d48c55
cinguilherme <cinguilherme@gmail.com>
2019-01-23 17:38:51 -0200

    smoke tests added

---
+++ src/main.js
@@ -0,0 +1,21 @@
+const search = () => {
+
+}
+
+const searchAlbuns = () => {
+
+}
+
+const searchArtits = () => {
+
+}
+
+const searchPlaylist = () => {
+
+}
+
+const searchTracks = () => {
+
+}
+
+export { search, searchAlbuns, searchArtits, searchPlaylist, searchTracks };
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,28 @@
 import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import { search, searchAlbuns, searchArtits, searchPlaylist, searchTracks } from '../src/main';
 
-describe('Main', () => {
+describe('Spotify Wrapper', () => {
+  describe('Smoke tests', () => {
 
+    it('should exists the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exists the search methodAlbuns', () => {
+      expect(searchAlbuns).to.exist;
+    });
+
+    it('should exists the searchArtits method', () => {
+      expect(searchArtits).to.exist;
+    });
+
+    it('should exists the searchPlaylist method', () => {
+      expect(searchPlaylist).to.exist;
+    });
+
+    it('should exists the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+  });
 });
